014abe393f610b5e3fb07efd9a442f6c9768f966
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-09-19 23:59:46 +0530

    Update RemoveChracterFromFirstString.java

--- String/Basics/RemoveChracterFromFirstString.java
+++ String/Basics/RemoveChracterFromFirstString.java
@@ -2,7 +2,8 @@ package GeeksForGeeks.String.Basics;
 
 public class RemoveChracterFromFirstString {
     static String removeChars(String str1, String str2) {
-        // code here
+        // Q. Given two strings str1 and str2, remove those characters from the first string(str1) which are present in the second string(str2).
+        // Both the strings are different and contain only lowercase characters.
         String str = "";
         for(int i=0;i<str1.length();i++){
             int flag =0;
