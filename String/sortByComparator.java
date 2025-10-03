4d8dad21166a631640f3a7247a3bfd9591f6d6f4
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-10-03 09:25:21 +0530

    Create sortByComparator.java

---
+++ String/sortByComparator.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void sortByComparator(String[] arr) {
+        // Your code here
+        
+        Arrays.sort(arr, String::compareToIgnoreCase);
+
+        // You sort it in a single line using method references
+    }
+}
