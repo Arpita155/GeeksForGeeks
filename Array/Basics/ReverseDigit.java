d9d1fc63e89b2748fa72318aba0dcafa5d33d436
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-10-26 20:57:45 +0530

    Add ReverseDigit class to reverse integer digits

---
+++ Array/Basics/ReverseDigit.java
@@ -0,0 +1,18 @@
+public class ReverseDigit{
+    
+    public static int reverseDigits(int n) {
+        // Code here
+        int rev = 0;
+        while(n!=0){
+            int rem = n%10;
+            rev = rev*10 + rem;
+            n/=10;
+        }
+        return rev;
+    }
+    public static void main(String [] args){
+        int n = 122;
+        System.out.println(reverseDigits(n));
+    }
+}
+
