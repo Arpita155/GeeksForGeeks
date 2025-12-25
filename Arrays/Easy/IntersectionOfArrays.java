e0d0939366337ee7af82f1f4fd40f424ed1e90d4
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-12-25 19:42:27 +0530

    modified

--- Arrays/Easy/IntersectionOfArrays.java
+++ Arrays/Easy/IntersectionOfArrays.java
@@ -2,6 +2,9 @@ package GeeksForGeeks.Arrays.Easy;
 
 import java.util.HashSet;
 
+//Q. Given two unsorted integer arrays a[] and b[] each consisting of distinct elements, the task is to return the
+// count of elements in the intersection (or common elements) of the two arrays.
+
 public class IntersectionOfArrays {
 
     public static int intersectSize(int a[], int b[]) {
