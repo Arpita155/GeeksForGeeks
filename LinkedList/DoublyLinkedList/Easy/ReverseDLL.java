66a6abc55cb56d7729da949f33073f9bed623896
Arpita Sahoo <arpitasahoo795@gmail.com>
2026-05-09 22:26:38 +0530

    msg

--- LinkedList/DoublyLinkedList/Easy/ReverseDLL.java
+++ LinkedList/DoublyLinkedList/Easy/ReverseDLL.java
@@ -3,20 +3,18 @@ package GeeksForGeeks.LinkedList.DoublyLinkedList.Easy;
 public class ReverseDLL {
 
     public static Node reverse(Node head) {
-        // code here
-        if(head == null || head.next == null)
-        {
+        if(head == null || head.next == null) {
             return head;
         }
-        Node temp = null;
         Node curr = head;
-        while(curr!=null)
-        {
+        Node temp = null;
+        while(curr!=null) {
             temp = curr.prev;
             curr.prev = curr.next;
             curr.next = temp;
             curr = curr.prev;
         }
+
         if(temp != null){
             head = temp.prev;
         }
@@ -25,16 +23,23 @@ public class ReverseDLL {
 
     public static void main(String[] args) {
         Node head = new Node(10);
-        head.next=new Node(20);
-        head.next.next=new Node(30);
-        head.next.next.next=new Node(40);
+        Node p1 =new Node(20);
+        Node p2=new Node(30);
+        Node p3=new Node(40);
+        p1.prev=head;
+        head.next=p1;
+        p2.prev=p1;
+        p1.next=p2;
+        p3.prev=p2;
+        p2.next=p3;
 
-        System.out.print("Original LL : ");
+
+        System.out.print("Original DLL : ");
         Node.traversal(head);
 
         head = reverse(head);
 
-        System.out.print("After reversing : ");
+        System.out.print("\nAfter reversing DLL : ");   // After reversing DLL : 40 30 20 10
         Node.traversal(head);
     }
 }
